bcf852e3ca032c2ff50c66672612f14c54359053
elena-bonan <elena.bonan@king.com>
2021-03-06 23:14:53 +0100

    visualization of cnn

--- Food_recognition/Custom_functions/plots.py
+++ Food_recognition/Custom_functions/plots.py
@@ -59,3 +59,49 @@ def plot_acc_loss_smooth(history):
     plt.title('Training and validation loss')
     plt.legend()
     plt.show()
+    
+
+def deprocess_image(x):
+    """ Function to normalize the values of the image"""
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x,0,1)
+    x *= 255
+    x = np.clip(x,0,255).astype('uint8')
+    return x
+
+
+def generate_pattern(model, layer_name, filter_index, size = 150):
+    """This function create the image which maximize the specify filter"""
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:,:,:,filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5) 
+    iterate = K.function([model.input],[loss, grads])
+    input_img_data = np.random.random((1,150,150,3)) * 20 +128
+    step = 1.
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+
+def plot_filters (model,layer_name):
+    """This function print 64 filters for the specified layer"""
+    size = 150
+    margin = 5
+    results = np.zeros((8* size + 7 * margin, 8 * size + 7* margin,3))
+    for i in range(8):
+        for j in range(8):
+            filter_img = generate_pattern(model, layer_name, i + (j*8), size = size)
+            horizontal_start = i * size + i * margin
+            horizontal_end = horizontal_start + size
+            vertical_start = j * size + j * margin
+            vertical_end = vertical_start + size
+            results[horizontal_start: horizontal_end, vertical_start: vertical_end,:] = filter_img
+    plt.figure(figsize = (20,20))
+    results = results.astype(int)
+    plt.imshow(results)
